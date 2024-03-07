62366bd8bc0622af9ba6db83bc2ccca4e22ed191
SCARECROW <100871554+scarecrowgit@users.noreply.github.com>
2024-03-08 02:00:41 +0300

    style: fields marked down
    need to make it active (it only md)

--- src/components/Sign-In-comp.js
+++ src/components/Sign-In-comp.js
@@ -5,10 +5,23 @@ class SignInComp extends React.Component {
     render() {
       return (
         <body>
-            <div className='heading-container'>
-                <h1 id="heading">SIGN IN</h1>
+            <div className='mainContainer'>
+                <div className='heading-container'>
+                    <h1 id="heading">SIGN IN</h1>
+                </div>
+                <div className='InputFieldsContainer'>
+                    <div className='usernameField'>
+                        <div className='userIField'>
+                            <p id="usernamePlaceholder">username</p>
+                        </div>
+                    </div>
+                    <div className='passwordField'>
+                        <div className='passwordIField'>
+                            <p id="passwordPlaceholder">password</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          
         </body>
       );
     }
