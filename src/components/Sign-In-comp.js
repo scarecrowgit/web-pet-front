655494f9266380154f21e66531c2f3c421865fe3
SCARECROW <100871554+scarecrowgit@users.noreply.github.com>
2024-03-08 01:15:05 +0300

    style: sign in component init and header

---
+++ src/components/Sign-In-comp.js
@@ -0,0 +1,17 @@
+import './Sign-In-comp.css';
+import React from 'react';
+
+class SignInComp extends React.Component {
+    render() {
+      return (
+        <body>
+            <div className='heading-container'>
+                <h1 id="heading">SIGN IN</h1>
+            </div>
+          
+        </body>
+      );
+    }
+  }
+
+  export default SignInComp;
