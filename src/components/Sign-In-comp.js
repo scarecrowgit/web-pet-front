e8228663d95d250415c83828595dfb714b953327
SCARECROW <100871554+scarecrowgit@users.noreply.github.com>
2024-03-08 11:54:12 +0300

    feat: input fields & handling for it

--- src/components/Sign-In-comp.js
+++ src/components/Sign-In-comp.js
@@ -1,30 +1,50 @@
 import './Sign-In-comp.css';
-import React from 'react';
+import React, {useState} from 'react';
 
-class SignInComp extends React.Component {
-    render() {
-      return (
-        <body>
-            <div className='mainContainer'>
-                <div className='heading-container'>
-                    <h1 id="heading">SIGN IN</h1>
-                </div>
-                <div className='InputFieldsContainer'>
-                    <div className='usernameField'>
-                        <div className='userIField'>
-                            <p id="usernamePlaceholder">username</p>
-                        </div>
+
+const SignInComp = () => {
+    const [username, setusername] = useState('');
+    const [password, setpassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setusername(e.target.value);
+      };
+
+    const handlePasswordChange = (e) => {
+        setpassword(e.target.value);
+    };
+    
+    
+    return (
+    <body>
+        <div className='mainContainer'>
+            <div className='heading-container'>
+                <h1 id="heading">SIGN IN</h1>
+            </div>
+            <div className='InputFieldsContainer'>
+                <div className='usernameField'>
+                    <div className='userIField'>
+                        <input type="text" value = { username } id="username" onChange={handleUsernameChange}/>
+                        {username === '' && (
+                            <p id="usernamePlaceholder">{'username'}</p>
+                        )}
                     </div>
-                    <div className='passwordField'>
-                        <div className='passwordIField'>
-                            <p id="passwordPlaceholder">password</p>
-                        </div>
+                </div>
+                <div className='passwordField'>
+                    <div className='passwordIField'>
+                        <input type="text" value = { password } id="password" onChange={handlePasswordChange}/>
+                        {password === '' && (
+                            <p id="passwordPlaceholder">{'password'}</p>
+                        )}
                     </div>
                 </div>
             </div>
-        </body>
-      );
-    }
+            <div className='buttonContainer'>
+                
+            </div>
+        </div>
+    </body>
+    );
   }
 
   export default SignInComp;
